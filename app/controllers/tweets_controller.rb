a0d51f5d807ff766d604ff1f42f3e83aab02c316
normancapule <jndc6@yahoo.com>
2011-05-20 17:43:51 +0800

    Update: - added AJAX in forms

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,9 +7,9 @@ class TweetsController < ApplicationController
 
   def create
     @user = User.find(params[:user_id])
-    @user.tweets.build(params[:tweet])
+    @tweet = @user.tweets.build(params[:tweet])
     @user.save
-    redirect_to admin_path
+    refresh
   end
 
   def edit
@@ -22,7 +22,21 @@ class TweetsController < ApplicationController
     @user = User.find(params[:user_id])
     @tweet = @user.tweets.find params[:id]
     @tweet.destroy
-    redirect_to admin_path
+    refresh
   end
-
+  
+  private
+    def refresh
+      respond_to do |format| #for ajax
+        format.html {redirect_to admin_path}#tells the request that it is an html
+        format.js do 
+          @tweets = @user.tweets
+          render :update do |page|
+            page.replace_html 'tweets', :partial=>'/admin/tweet' 
+            page[:new_tweet].reset
+          end
+        end #communicates with comments/create.rjs (js because of this Processing 
+            #by CommentsController#create as JS in the logs
+      end
+    end
 end
