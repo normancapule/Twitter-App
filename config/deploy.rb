3311e750f97ecbc572546e05e71e383eadd28365
Normz <normz@ubuntu.(none)>
2011-05-21 01:31:01 +0800

    Test Update

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,19 +1,13 @@
 set :application, "twitter"
-set :repository,  "https://normancapule@github.com/normancapule/Twitter-App.git"
-	
+set :repository,  "https://normancapule@github.com/normancapule/Twitter-App.git" #direct address of the app
 set :scm, :git
-set :deploy_to,"/home/exist/Dropbox/Exist/cap"
+set :deploy_to,"/home/normz/Documents/Exist-Code-Camp/cap"
 default_run_options[:pty] = true
 server "localhost", :app, :web, :db, :primary => true
-
 #role :web, "your web-server here"                          # Your HTTP server, Apache/etc
 #role :app, "your app-server here"                          # This may be the same as your `Web` server
 #role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
 #role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
    task :start do ; end
